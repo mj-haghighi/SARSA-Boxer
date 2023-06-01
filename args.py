1bb0b9428f60bd079d15823731d24689af9f22a2
mj-haghighi <mohammadjavad.developer@gmail.com>
2023-06-01 12:41:26 +0330

    Add some args to train or infer box agent

---
+++ args.py
@@ -0,0 +1,18 @@
+
+
+import argparse
+
+
+def get_args():
+    parser = argparse.ArgumentParser(description='Would you like to train boxer or figth?')
+
+    # Add arguments
+    parser.add_argument('--train', action='store_true',help='start to train boxer')
+    parser.add_argument('--resume', action='store_true',help='resume training')
+    parser.add_argument('--q_table', type=str, help='path to q-table')
+    parser.add_argument('--fight', action='store_true', help='start to fight!')
+
+    # Parse the arguments
+    args = parser.parse_args()
+
+    return args
