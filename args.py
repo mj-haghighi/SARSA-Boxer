4ae556a79afc108bfbd4a921310dd7dadd520ad0
mj-haghighi <mohammadjavad.developer@gmail.com>
2023-06-01 14:57:42 +0330

    Add 'random' to args

--- args.py
+++ args.py
@@ -1,5 +1,3 @@
-
-
 import argparse
 
 
@@ -11,6 +9,7 @@ def get_args():
     parser.add_argument('--resume', action='store_true',help='resume training')
     parser.add_argument('--q_table', type=str, help='path to q-table')
     parser.add_argument('--fight', action='store_true', help='start to fight!')
+    parser.add_argument('--random', action='store_true', help='start to act randomly!')
 
     # Parse the arguments
     args = parser.parse_args()
