19c00be039078c28876766d28b1f2b85ebee581c
mj-haghighi <mohammadjavad.developer@gmail.com>
2023-06-01 15:02:14 +0330

    Use boxing agent, random, training and fighter

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
-from .args import get_args
-from .agent import Boxer
+import sys
+sys.path.append('../')
 
+from args import get_args
+from agent import TrainingBoxer, FighterBoxer, RandomBoxer
+from game import Game
 
 if __name__ == "__main__":
     args = get_args()
-    boxer = Boxer(q_table_path=args.q_table)
 
     if args.train:
-        boxer.train(args.resume)
+        boxer = TrainingBoxer(q_table_path=args.q_table)
     elif args.fight:
-        boxer.fight()
+        boxer = FighterBoxer(q_table_path=args.q_table)
+    elif args.random:
+        boxer = RandomBoxer(q_table_path=args.q_table)
     else:
         raise Exception('train or fight? use one of `--train` `--fight`')
+
+    game = Game(boxer=boxer)
+    game.start()
