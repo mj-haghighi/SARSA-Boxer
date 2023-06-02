882133b560c3dc8e4f7257134532d761fd2e18dc
mj-haghighi <mohammadjavad.developer@gmail.com>
2023-06-02 15:26:37 +0330

    Add monitoring controle in game loop

--- game.py
+++ game.py
@@ -2,37 +2,39 @@ import pickle
 import gymnasium as gym
 from agent import Boxer
 
-
 class Game:
     def __init__(self, boxer: Boxer) -> None:
         self.env = gym.make("ALE/Boxing-v5")
         self.boxer = boxer
-        self.boxer.set_action_space(self.env.action_space)
+        self.boxer.action_space = self.env.action_space
 
     def start(self, episods=2000):
         observation, info = self.env.reset(seed=42)
         reward_history = []
-        for i in range(episods):
+        for e in range(episods):
             terminated, truncated = False, False
             observation, info = self.env.reset()
             total_reward = 0
             reward = 0
-            step = 0
             while not (terminated or truncated):
-                action = self.boxer.take_action(
-                    observation, reward, self.env.action_space, i)
+                action = self.boxer.take_action(observation, reward)
                 step_result = self.env.step(action)
                 observation, reward, terminated, truncated, info = step_result
                 total_reward += reward
-                step += 1
+            
+            print(self.boxer.q_table)
+            print(f'episode: {e}, reward: {total_reward}, {self.boxer.params}')
+
+            self.boxer.params.decay_epsilon()
+            self.boxer.params.decay_alpha()
             reward_history.append(total_reward)
             
-            self.boxer.get_q_sum()
-            print(f'episode: {i}, reward: {total_reward}, epsilon: {self.boxer.epsilon}')
-            
+            if e % 10 == 0:
+                with open(f'checkpoints/Q2-Table{e}.pickle', 'wb') as handle:
+                    pickle.dump(self.boxer.q_table, handle,
+                                protocol=pickle.HIGHEST_PROTOCOL)
+                with open(f'checkpoints/Rewards.pickle{e}', 'wb') as handle:
+                    pickle.dump(reward_history, handle,
+                                protocol=pickle.HIGHEST_PROTOCOL)
         self.env.close()
-        with open('Q2-Table.pickle', 'wb') as handle:
-            pickle.dump(self.boxer.q_table, handle,
-                        protocol=pickle.HIGHEST_PROTOCOL)
 
-        print(reward_history)
