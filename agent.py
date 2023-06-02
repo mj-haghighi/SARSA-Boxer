b7d9ace9c934a85e8e0f0356670c72fbd3dcac9b
mj-haghighi <mohammadjavad.developer@gmail.com>
2023-06-02 15:22:46 +0330

    Refactor 'Boxer' and create new class 'QTabel'

--- agent.py
+++ agent.py
@@ -1,7 +1,9 @@
 import os
 import pickle
 import numpy as np
+from qtable import QTable
 from img_utils import find_heads, encode_state
+from hyperparameter import Hyperparameters
 
 
 class Boxer:
@@ -9,86 +11,60 @@ class Boxer:
     Boxer agnet class
     """
 
-    def __init__(self, q_table_path: str) -> None:
-        self.q_table = {}
-        if q_table_path is not None and os.path.exists(q_table_path):
-            self.q_table = pickle.load(q_table_path)
-        self.action_space = None
+    def __init__(self, q_table: QTable = None, params: Hyperparameters = None) -> None:
+        self.__action_space = None
+        self.q_table = q_table
+        self.params = params
 
-    def take_action(self, observation, reward, info, action_space, episode):
+    @property
+    def action_space(self):
+        return self.__action_space
+
+    @action_space.setter
+    def action_space(self, value):
+        self.__action_space = value
+        self.q_table.num_available_actions = value.n
+
+    def take_action(self, observation, reward):
         raise Exception("This method os not imlemented")
-
-    def mine_q_table(self, state):
-        if state in self.q_table.keys():
-            action, score = np.argmax(self.q_table[state]), np.max(self.q_table[state])
-            return action
-        return self.action_space.sample()
-
-    def get_score(self, state, action):
-        if state in self.q_table.keys():
-            return self.q_table[state][action]
-        return 0
-
-    def get_q_sum(self):
-        total=0
-        for key, val in self.q_table.items():
-            print(key, np.mean(val), np.max(val))
-
-
-    def set_score(self, state, action, score):
-        if state not in self.q_table.keys():
-            self.q_table[state] = np.zeros(shape=(self.action_space.n, ))
-        self.q_table[state][action] = score
-
-    def set_action_space(self, actions_space):
-        self.action_space = actions_space
-
-    def choose_action(self, state, epsilon=0.1):
-        """ Chose action
-        """
-        action = 0
-        if np.random.uniform(0, 1) > epsilon:
-            action = self.mine_q_table(state)
-        else:
-            action = self.action_space.sample()
-
-        return action
 
 
 class RandomBoxer(Boxer):
-    def take_action(self, observation, reward, info, action_space):
-        return action_space.sample()
+    def __init__(self) -> None:
+        super().__init__(None, None)
+
+    def take_action(self, observation, reward):
+        return self.action_space.sample()
 
 
 class TrainingBoxer(Boxer):
-    def __init__(self, q_table_path: str, resume: bool = False, gamma=0.9, alpha=0.8) -> None:
-        super().__init__(q_table_path)
+    def __init__(self, q_table: QTable = None, params: Hyperparameters = None, resume: bool = False) -> None:
+        super().__init__(q_table, params)
         self.resume = resume
-        self.gamma = gamma
-        self.alpha = alpha
-        self.epsilon = 1
-
         self.state = None
         self.action = None
 
-    def update(self, state, state2, reward, action, action2):
+    def update(self, new_state, new_action, reward):
         """
         Function to learn the Q-value
         """
+        predict = self.q_table.get_score(self.state, self.action)
+        target = reward + self.params.gamma.value *\
+            self.q_table.get_score(new_state, new_action)
+        score = predict + self.params.alpha.value * (target - predict)
+        self.q_table.set_score(self.state, self.action, score)
 
-        predict = self.get_score(state, action)
-        target = reward + self.gamma * self.get_score(state2, action2)
-        score = self.get_score(state, action) + self.alpha * (target - predict)
-        self.set_score(state, action, score)
-
-    def take_action(self, observation, reward, info, episode=1):
+    def take_action(self, observation, reward):
         boxer, enemy = find_heads(image=observation)
         new_state = encode_state(boxer, enemy)
-        self.epsilon = max(0.1, 1 - episode * 0.0005)
-        new_action = self.choose_action(new_state, self.epsilon)
+        new_action = None
+        if np.random.uniform(0, 1) > self.params.epsilon.value:
+            new_action = self.q_table.mine(new_state)
+        if new_action is None:
+            new_action = self.action_space.sample()
 
         if self.state is not None:
-            self.update(self.state, new_state, reward, self.action, new_action)
+            self.update(new_state, new_action, reward)
 
         self.action = new_action
         self.state = new_state
---
+++ qtable.py
@@ -0,0 +1,34 @@
+import os
+import pickle
+import numpy as np
+
+
+class QTable:
+    def __init__(self, path, num_available_actions: int = None) -> None:
+        self.table = {}
+        if path is not None and os.path.exists(path):
+            self.table = pickle.load(path)
+        self.num_available_actions = num_available_actions
+
+    def mine(self, state):
+        if state in self.table.keys():
+            action = np.argmax(self.table[state])
+            return action
+        return None
+
+    def get_score(self, state, action):
+        if state in self.table.keys():
+            return self.table[state][action]
+        return 0
+
+    def set_score(self, state, action, score):
+        if state not in self.table.keys():
+            self.table[state] = np.zeros(shape=(self.num_available_actions, ))
+        self.table[state][action] = score
+
+
+    def __str__(self) -> str:
+        res = ""
+        for state, scores in self.table.items():
+            res += "\n{} | mean: {:.4f} | max: {:.4f}".format(state, np.mean(scores), np.max(scores))
+        return str(res)
