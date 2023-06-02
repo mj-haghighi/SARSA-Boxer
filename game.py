7e625d67c1c43c89210138a5e3d90d0ac7f10e7c
mj-haghighi <mohammadjavad.developer@gmail.com>
2023-06-02 10:23:56 +0330

    Continue game until the end that 'terminated or truncated' set false at enviroment update

--- game.py
+++ game.py
@@ -1,20 +1,38 @@
+import pickle
 import gymnasium as gym
 from agent import Boxer
 
 
 class Game:
     def __init__(self, boxer: Boxer) -> None:
-        self.env = gym.make("ALE/Boxing-v5", render_mode="human")
+        self.env = gym.make("ALE/Boxing-v5")
         self.boxer = boxer
+        self.boxer.set_action_space(self.env.action_space)
 
-    def start(self, step=1000):
+    def start(self, episods=2000):
         observation, info = self.env.reset(seed=42)
-        reward = 0
-        for _ in range(step):
-            action = self.boxer.take_action(
-                observation, reward, info, self.env.action_space)
-            step_result = self.env.step(action)
-            observation, reward, terminated, truncated, info = step_result
-            if terminated or truncated:
-                observation, info = self.env.reset()
+        reward_history = []
+        for i in range(episods):
+            terminated, truncated = False, False
+            observation, info = self.env.reset()
+            total_reward = 0
+            reward = 0
+            step = 0
+            while not (terminated or truncated):
+                action = self.boxer.take_action(
+                    observation, reward, self.env.action_space, i)
+                step_result = self.env.step(action)
+                observation, reward, terminated, truncated, info = step_result
+                total_reward += reward
+                step += 1
+            reward_history.append(total_reward)
+            
+            self.boxer.get_q_sum()
+            print(f'episode: {i}, reward: {total_reward}, epsilon: {self.boxer.epsilon}')
+            
         self.env.close()
+        with open('Q2-Table.pickle', 'wb') as handle:
+            pickle.dump(self.boxer.q_table, handle,
+                        protocol=pickle.HIGHEST_PROTOCOL)
+
+        print(reward_history)
