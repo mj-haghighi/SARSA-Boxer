9878d14179c57c2ac22b1cd975b1595ae38bcbf2
mj-haghighi <mohammadjavad.developer@gmail.com>
2023-06-02 15:24:34 +0330

    Add 'gamma', 'alpha', 'epsilon' to arguments

--- args.py
+++ args.py
@@ -10,6 +10,9 @@ def get_args():
     parser.add_argument('--q_table', type=str, help='path to q-table')
     parser.add_argument('--fight', action='store_true', help='start to fight!')
     parser.add_argument('--random', action='store_true', help='start to act randomly!')
+    parser.add_argument('--gamma', type=float, default=0.9, help='gamma | hyper parameter')
+    parser.add_argument('--alpha', type=float, default=0.8, help='learning rate')
+    parser.add_argument('--epsilon', type=float, default=1.0, help='epsilon gready')
 
     # Parse the arguments
     args = parser.parse_args()
