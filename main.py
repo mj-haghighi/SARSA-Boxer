aeb928ee85839f5769dc769c5d8800621493f59c
mj-haghighi <mohammadjavad.developer@gmail.com>
2023-06-02 15:26:06 +0330

    Refactor 'main.py' with respect to agent and game

--- main.py
+++ main.py
@@ -4,16 +4,21 @@ sys.path.append('../')
 from args import get_args
 from agent import TrainingBoxer, FighterBoxer, RandomBoxer
 from game import Game
+from qtable import QTable
+from hyperparameter import Hyperparameters
 
 if __name__ == "__main__":
     args = get_args()
 
     if args.train:
-        boxer = TrainingBoxer(q_table_path=args.q_table)
+        q_table = QTable(path=args.q_table)
+        params = Hyperparameters(gamma=args.gamma, alpha=args.alpha, epsilon=args.epsilon)
+        boxer = TrainingBoxer(q_table=q_table, params=params,resume=args.resume)
     elif args.fight:
-        boxer = FighterBoxer(q_table_path=args.q_table)
+        q_table = QTable(path=args.q_table)
+        boxer = FighterBoxer(q_table=q_table)
     elif args.random:
-        boxer = RandomBoxer(q_table_path=args.q_table)
+        boxer = RandomBoxer()
     else:
         raise Exception('train or fight? use one of `--train` `--fight`')
 
